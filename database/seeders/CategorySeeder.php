8fc122869dc00fee76332ba9afa363b9d6bf94ef
Alisher Eshmurodov <eshmurodov.cooder@gmail.com>
2023-09-29 08:09:45 +0500

    add catContr

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,30 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            "name" => [
+                "en" => "Chair",
+                "ru" => "стул",
+            ],
+        ]);
+        Category::create([
+            "name" => [
+                "en" => "Table",
+                "ru" => "стол",
+            ],
+        ]);
+        Category::create([
+            "name" => [
+                "en" => "Armchair",
+                "ru" => "кресло",
+            ],
+        ]);
+        Category::create([
+            "name" => [
+                "en" => "Bed",
+                "ru" => "кровать",
+            ],
+        ]);
+
     }
 }
